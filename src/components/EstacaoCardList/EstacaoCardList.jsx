a28059e277ccaca0417c8cb352fbd0ca5e1bbc2a
VMonteiro <valmirmjunior@gmail.com>
2026-01-30 14:40:14 -0300

    header lista de estações

--- src/components/EstacaoCardList/EstacaoCardList.jsx
+++ src/components/EstacaoCardList/EstacaoCardList.jsx
@@ -4,7 +4,7 @@ import { catalogoEstacoes } from "../../data/estacoes";
 export default function EstacaoCardList() {
   return (
     <>
-      <h2>Lista de Estações</h2>
+      <h2>Estações Ativas</h2>
       <div>
         {catalogoEstacoes.getEstacoesAtivas().map((estacao) =>
             <EstacaoCard key={estacao.id+"_card"} stationId={estacao.id}>{estacao.bairro}</EstacaoCard>
