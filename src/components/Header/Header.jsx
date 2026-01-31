8960ab89f97cd0b68ccba649bc0005c2e8d24c1a
VMonteiro <valmirmjunior@gmail.com>
2026-01-31 09:56:26 -0300

    adiconado ícone no header e meta tags

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import "./Header.css";
+import image from '../../assets/thermometer.png';
 
 export default function Header({ menuSelecionado, selecionarMenu }) {
   return (
     <header className="header-app">
+      <img src={image} alt="Logo SIM-IC-Macae" className="logo-termometro" />
       <h1 className="titulo-app">Índice de Calor em Macaé/Rj </h1>
       <nav className="menu-navegacao">
         <button onClick={() => selecionarMenu("inicio")} disabled={menuSelecionado === "inicio"}>Início</button>
